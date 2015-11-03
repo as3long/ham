f1dbefb348a0888f42e955a3dcef7fa3d8a7a94c
qq49121294 <49121294@163.com>
2015-11-04 00:55:06 +0800

    错误上报

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ requirejs.config({
     baseUrl:"/js"
 });
 
-define(["duoshuo","jquery.lazyloadWebp","prism.full.min","baidu.tongji"],function(duoshuo,lazyloadWebp,prism,tongji){
+define(["duoshuo","jquery.lazyloadWebp","baidu.tongji"],function(duoshuo,lazyloadWebp,tongji){
     window.duoshuoQuery = {short_name:"ham"};
     $(function(){
         $("img").lazyloadWebp({
