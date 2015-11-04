663ac30fcce9e4c69e1ae64a26df6d5ae506b50c
qq49121294 <49121294@163.com>
2015-11-04 22:15:25 +0800

    page.js

---
+++ ham/controller/page.js
@@ -0,0 +1,19 @@
+var marked = require("marked");
+module.exports = Ham.Controller("base",{
+    indexAction: function () {
+        this.display();
+    },
+    _404Action:function(){
+        var action=this.req.action;
+        Ham.model.table("contents").where({type:"page",status:"publish",slug:action}).find().then(function(data){
+            data = data[0];
+            if(data){
+                data.text = marked(data.text);
+                self.assign("content", data);
+                self.display("/archives/blog");
+            }else{
+                Ham._404Action(self.req,self.res);
+            }
+        })
+    }
+}
