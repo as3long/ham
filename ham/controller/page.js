1b00aba137ab99a2d2a0f983ca49e53559c8eff2
qq49121294 <49121294@163.com>
2015-11-04 22:19:52 +0800

    page.js

--- ham/controller/page.js
+++ ham/controller/page.js
@@ -1,18 +1,23 @@
 var marked = require("marked");
-module.exports = Ham.Controller("base",{
+module.exports = Ham.Controller("base", {
     indexAction: function () {
         this.display();
     },
-    _404Action:function(){
-        var action=this.req.action;
-        Ham.model.table("contents").where({type:"page",status:"publish",slug:action}).find().then(function(data){
+    _404Action: function () {
+        var self = this,
+            action = this.req.action;
+        Ham.model.table("contents").where({
+            type: "page",
+            status: "publish",
+            slug: action
+        }).find().then(function (data) {
             data = data[0];
-            if(data){
+            if (data) {
                 data.text = marked(data.text);
                 self.assign("content", data);
                 self.display("/archives/blog");
-            }else{
-                Ham._404Action(self.req,self.res);
+            } else {
+                Ham._404Action(self.req, self.res);
             }
         })
     }
